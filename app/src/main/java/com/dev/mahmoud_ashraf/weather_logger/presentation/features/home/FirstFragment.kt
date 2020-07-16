7348f34de3bb80c79841ba4d543ef3b039c83005
dev_mahmoud_ashraf <ashraaf.mahmoud@gmail.com>
2020-07-16 11:22:35 +0200

    add locationApi class

--- app/src/main/java/com/dev/mahmoud_ashraf/weather_logger/presentation/features/home/FirstFragment.kt
+++ app/src/main/java/com/dev/mahmoud_ashraf/weather_logger/presentation/features/home/FirstFragment.kt
@@ -1,4 +1,4 @@
-package com.dev.mahmoud_ashraf.weather_logger
+package com.dev.mahmoud_ashraf.weather_logger.presentation.features.home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.dev.mahmoud_ashraf.weather_logger.R
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
--- app/src/main/java/com/dev/mahmoud_ashraf/weather_logger/presentation/features/home/MainActivity.kt
+++ app/src/main/java/com/dev/mahmoud_ashraf/weather_logger/presentation/features/home/MainActivity.kt
@@ -1,11 +1,15 @@
-package com.dev.mahmoud_ashraf.weather_logger
+package com.dev.mahmoud_ashraf.weather_logger.presentation.features.home
 
+import android.location.Location
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.dev.mahmoud_ashraf.weather_logger.R
+import com.dev.mahmoud_ashraf.weather_logger.presentation.features.core.LocationApi
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +18,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+
+
+
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+
+            LocationApi(this,callbacks = object :LocationApi.Callbacks{
+                override fun onSuccess(location: Location) {
+                    Timber.e("done "+location.latitude)
+                }
+
+                override fun onFailed(locationFailedEnum: LocationApi.LocationFailedEnum) {
+                    Timber.e(locationFailedEnum.name)
+                }
+
+            })
+
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
